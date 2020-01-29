185893d3739db7b1aa878bf74fd76c1636604719
Geefy <42178995+Geefy@users.noreply.github.com>
2020-01-29 10:01:51 +0100

    Add project files.

---
+++ HelpJacob/MessageHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJacob
+{
+    class MessageHandler
+    {
+        MessageSender messageSender;
+
+        void HandleMessage(Message m, bool isHTML, MessageCarrier carrier)
+        {
+            SetMessageSender(carrier);
+
+            if (isHTML)
+                m.Body = HtmlConverter.ConvertBodyToHTML(m.Body);
+
+            messageSender.sendMessage(m);
+        }
+        void HandleMessage(string[] to, Message m, bool isHTML, MessageCarrier carrier)
+        {
+            SetMessageSender(carrier);
+
+            if (isHTML)
+                m.Body = HtmlConverter.ConvertBodyToHTML(m.Body);
+
+            foreach (string person in to)
+            {
+                m.To = person;
+                messageSender.sendMessage(m);
+            }
+        }
+        public void SetMessageSender(MessageCarrier carrier)
+        {
+
+            switch (carrier)
+            {
+                case MessageCarrier.Smtp:
+                    messageSender = new SMTPSender();
+                    break;
+                case MessageCarrier.VMessage:
+                    messageSender = new SMTPSender();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
---
+++ HelpJacob/VMessageSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJacob
+{
+    class VMessageSender : MessageSender
+    {
+        public override void sendMessage(Message m)
+        {
+            //her implementeres alt koden til at sende via VMessage
+        }
+    }
+}
